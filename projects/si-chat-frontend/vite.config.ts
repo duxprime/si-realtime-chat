cb8853aa67f115dfedff62da8ef3dac1ed72d3aa
Chris Duxbury <dux.chris@gmail.com>
2024-01-27 16:31:48 -0500

    feat(si-chat): support TS decorators and reflection

--- projects/si-chat-frontend/vite.config.ts
+++ projects/si-chat-frontend/vite.config.ts
@@ -1,18 +1,43 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { vitePluginTypescriptTransform } from 'vite-plugin-typescript-transform';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true
+  },
   plugins: [
     vue(),
     vueJsx(),
+    vitePluginTypescriptTransform({
+      enforce: 'pre',
+      filter: {
+        files: {
+          include: /\.ts$/,
+        },
+      },
+      tsconfig : {
+        location: 'tsconfig.node.json'
+      }
+    })
   ],
+  optimizeDeps: {
+    esbuildOptions: {
+      tsconfigRaw: {
+        compilerOptions : {
+          experimentalDecorators: true,
+          // @ts-ignore
+          emitDecoratorMetadata: true, 
+        }
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
 })
