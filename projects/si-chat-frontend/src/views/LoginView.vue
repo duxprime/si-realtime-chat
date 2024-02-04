f8be2570d047d0d193ebb42cd14c61817c0100f9
Chris Duxbury <dux.chris@gmail.com>
2024-02-04 18:39:05 -0500

    feat(si-chat): implement login view

---
+++ projects/si-chat-frontend/src/views/LoginView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+    import { LoginComponent } from 'presentation/login';
+    import { useRouter } from 'vue-router'
+    import { Route } from '../setup';
+
+    const router = useRouter();
+    function navigateHome(){
+        router.push(Route.Home);
+    }
+</script>
+
+<template>
+    <div class="flex justify-center items-center">
+        <LoginComponent @login-complete="navigateHome()" class="w-1/2" greeting="Welcome to System Initiative!"></LoginComponent>
+    </div>
+</template>
