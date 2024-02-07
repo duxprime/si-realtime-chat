35d51f215bc739e7d5622767d9ecb1a24cbc195e
Chris Duxbury <dux.chris@gmail.com>
2024-02-06 22:01:08 -0500

    feat(si-chat): implement home/chat view

--- projects/si-chat-frontend/src/views/HomeView.vue
+++ projects/si-chat-frontend/src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+    import { ChatComponent } from 'presentation/chat';
+    import { UserListComponent } from 'presentation/user-list';
+    import { SiLogoIcon } from '../icons';
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <main>
+        <div class="flex flex-row h-full space-x-5">
+            <div flex-col class="space-y-5">
+                <SiLogoIcon class="w-fit bg-zinc-900" />
+                <UserListComponent id="user-list" class="max-h-[75%]"/>
+            </div>
+            <ChatComponent class="grow"/>
+        </div>
+    </main>
 </template>
